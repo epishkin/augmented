7d2e7e84abc439972aabf056aef4dc46c431e997
Nikita Leonov <nikita@applique.me>
2013-04-27 16:07:35 -0400

    Updated with leap.

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+var Leap = require('leapjs').Leap
 var app = require('express'),
     server = require('http').createServer(app),
     io = require('socket.io'),
@@ -5,13 +6,39 @@ var app = require('express'),
 
 server.listen(8880);
 
-io.sockets.on('connection', function (socket) {
-  socket.emit('object-added', { type: 'big-mac' });
-//  socket.emit('object-removed');
-//  socket.emit('rotate', { direction: -1 });
+var controllerOptions = {enableGestures: true};
+
+Leap.loop(controllerOptions, function(frame) {
+    if (frame.gestures.length) {
+        var gesture = frame.gestures[0];
+        if(gesture.state == 'start') {
+            switch(gesture) {
+                case 'swipe': {
+                    socket.emit('swipe', { direction: gesture.direction });
+                    break;
+                }
+                case 'tap': {
+                    socket.emit('tap');
+                    break;
+                }
+            }
+        }
+    } else {
+        if (frame.hands.length) {
+            console.log('finger '+frame.hands[0].direction);
+        }
+    }
+})
+
 /*
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-*/
-});
+
+ io.sockets.on('connection', function (socket) {
+ //  socket.emit('object-removed');
+ //  socket.emit('rotate', { direction: -1 });
+
+ socket.on('my other event', function (data) {
+ console.log(data);
+ });
+
+ });
+ */
