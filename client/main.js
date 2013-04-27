0af35cefc784051c77dff99fc74383ad27fe149e
epishkin <epishkin@gmail.com>
2013-04-27 19:49:25 -0400

    move selection with cursor keys

--- client/main.js
+++ client/main.js
@@ -1,8 +1,13 @@
 var width = 800,
     height = 600;
 
-var uiData = [];
-var allData = [{name:"About"},{name:"Price"},{name:"Nutrition"}];
+var amplitude = 400
+var realWidth = amplitude * 2;
+
+var uiData = [
+    {name:"One WTC", offset:100},
+    {name:"Empire State Bldg", offset:300},
+    {name:"Central Park", offset:500}];
 
 var svg = d3.select("body")
     .append("svg:svg")
@@ -10,81 +15,71 @@ var svg = d3.select("body")
     .attr("height", height)
     .append("svg:g");
 
-var origin = 3*Math.PI/2;
-var nodeGap = Math.PI / 3; //TODO: Count dynamically depends on amount of nodes
-var speed = 0;
-var slowDown = 0;
-var lastTimerValue = 0;
+var x = 0;
+var selectedNode = 1;
 
-var selectedNode = 0;
+function swipe() {
+    var size = uiData.length
+    var translated = Math.floor((x + amplitude) / realWidth * size);
+    selectedNode = translated;
 
-var NODES_TRANSITION_TIME = 1000;
+    update();
+    console.log("x: " + x + ", selected: " + selectedNode);
+}
 
-//TODO: Calculate depends on sensor data
-var orbitCenter = [width/2, height/2];
-var orbitRadiuses = [100, 50]
-
-function swipe(direction) {
+function moveFinger(dx) {
     return function(event) {
-        //Recalculate speed & slowdown
-        speed = direction*nodeGap/NODES_TRANSITION_TIME;
-        slowDown = direction*(nodeGap/NODES_TRANSITION_TIME - nodeGap/10);
-
-        selectedNode += direction;
-    };
+        x += dx;
+        if (x >= amplitude) {
+            x = amplitude-1;
+        } else if (x < -amplitude) {
+            x = -(amplitude-1);
+        }
+        swipe();
+    }
 }
 
 var socket = io.connect('http://localhost:8880');
-socket.on('object-added', function (data) {
-    var ellipse = svg.append("ellipse")
-        .attr("class", "orbit")
-        .attr("cx", orbitCenter[0])
-        .attr("cy", orbitCenter[1])
-        .attr("rx", orbitRadiuses[0])
-        .attr("ry", orbitRadiuses[1])
-        .style("stroke-opacity", 0).transition().style("stroke-opacity", 1);
 
-    uiData = allData;
-});
-
+//todo replace with socket handler
 d3.select('body').call(d3.keybinding()
-    .on('←', swipe(-1))
-    .on('→', swipe(1))
+    .on('←', moveFinger(-10))
+    .on('→', moveFinger(10))
 );
 
-d3.timer(function(elapsed) {
-    var timeSpan = elapsed - lastTimerValue;
-    lastTimerValue = elapsed;
+update();
 
-    origin += timeSpan*speed;
-    speed += timeSpan*slowDown;
-
-    //Update positions
+function update() {
     var nodes = d3.select("svg").selectAll(".node")
-        .data(uiData)
-        .attr("transform", function(d, i) {
-            var x = orbitRadiuses[0]*Math.cos(origin + i*nodeGap) + orbitCenter[0];
-            var y = orbitRadiuses[1]*Math.sin(origin + i*nodeGap) + orbitCenter[1];
+        .data(uiData);
 
-            return "translate("+x+","+y+")";
-        });
+    nodes.attr("transform", function(d, i) {
+        var x = d.offset;
+        var y = 200;
+
+        return "translate("+x+","+y+")";
+    });
+    nodes.transition().duration(200).style("stroke-opacity", function(d,i) {return i == selectedNode ? 1 : 0});
+
 
     //Add new nodes
     var node = nodes.enter().append("g")
         .attr("class", "node")
         .attr("transform", function(d, i) {
-            var x = orbitRadiuses[0]*Math.cos(origin + i*nodeGap) + orbitCenter[0];
-            var y = orbitRadiuses[1]*Math.sin(origin + i*nodeGap) + orbitCenter[1];
+            var x = d.offset;
+            var y = 200;
 
             return "translate("+x+","+y+")";
-        });
+        })
+        .style("stroke-opacity", function(d,i) {return i == selectedNode ? 1 : 0});
 
-    node.append("circle")
-        .attr("r", 0).transition().duration(500).delay(function(d,i) {return 300*i;}).attr("r",10);
+    node.append("polygon")
+        .attr("points", "0,0, 150,0, 150,100, 0,100");
 
     node.append("text")
+        .attr("dx", "10px")
         .attr("dy", "25px")
-        .attr("text-anchor", "middle")
-        .style("stroke-opacity", 0).transition().duration(2000).delay(function(d,i) {return 300*i;}).style("stroke-opacity", 1)
+        .attr("text-anchor", "left")
+        //.style("stroke-opacity", 0)
         .text(function(d) {return d.name;});
-});
+}
